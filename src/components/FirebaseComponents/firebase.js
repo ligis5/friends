7e0c3798ccf4b01779969994cc1f2db44b9cb3c8
Lygis <69323111+ligis5@users.noreply.github.com>
2021-03-08 18:50:52 +0200

    Update firebase.js

--- src/components/FirebaseComponents/firebase.js
+++ src/components/FirebaseComponents/firebase.js
@@ -4,13 +4,7 @@ import "firebase/storage";
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZ5LtkwMmGOXIiKKtWw4HHUg5e7RAWeTk",
-  authDomain: "friends-6f994.firebaseapp.com",
-  projectId: "friends-6f994",
-  storageBucket: "friends-6f994.appspot.com",
-  messagingSenderId: "472178657516",
-  appId: "1:472178657516:web:b503468f35c111b5e1bdc9",
-  measurementId: "G-9HVB9M7BJV",
+  ...
 };
 
 if (!firebase.apps.length) {
